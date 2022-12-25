9856a5e7bb930a5ad020eb9aeecf868b59486b9d
kongtianyi <kongtianyi@foxmail.com>
2022-12-25 17:40:59 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # wox-jsonviewer
+
 A wox plugin help you to get a good json view.
+
+Get JSON from clipboard then show it in Chrome.
+
+## Depends
+* Google Chrome
+* (Optional)Google Chrome JSON Plugin(choose either one)
+  * [JSONVue](https://chrome.google.com/webstore/detail/jsonvue/chklaanhfefbnpoihckbnefhakgolnmc)
+  * [JSON Viewer](https://chrome.google.com/webstore/detail/json-viewer/gbmdgpbipfallnflgajpaliibnhdgobh)
+  * [JSON Formatter](https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa)
+
+## Usage
+
+### Basic
+
+
+1. Copy a JSON object into your clipboard
+2. Input `json` then press enter key
+
+### Find Sub JSON Object
+
+
+1. Copy a JSON object into your clipboard
+2. Input `json`
+3. Input key to filter key list in drop-down list and press enter to fill in the search box
+4. Input `>` to determine a key path and go into the sub JSON Object(string formatted JSON object also can unfold)
+5. Repeat 3,4 when find the aimed hierarchy
+6. Press enter key
+
+## Tips
+1. Chrome JSON plugin needs to turn on local file switch.
+2. Mac version: [alfred-jsonviewer](https://github.com/kongtianyi/alfred-jsonviewer/blob/main/README.md)
---
+++ json_viewer.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+import json
+import webbrowser
+import pyperclip
+import re
+import os
+from typing import List
+from enum import Enum
+from LanguageBinding.wox import Wox, WoxAPI
+
+RESULT_TEMPLATE = {
+    'Title': '{}',
+    'SubTitle': None,
+    'IcoPath': 'ui/icon.png',
+    "JsonRPCAction": {
+        "method": "create_file_and_open",
+        "parameters": ['{}'],
+        "dontHideAfterAction": False
+    }
+}
+
+PREDICT_TEMPLATE = {
+    'Title': '{}',
+    'SubTitle': None,
+    'IcoPath': 'ui/icon.png',
+    "JsonRPCAction": {
+        "method": "change_query",
+        "parameters": ['{}'],
+        "dontHideAfterAction": True
+    }
+}
+
+PYTHON_JSON_TYPE_MAP = {
+    "dict": "object",
+    "list": "array",
+    "tuple": "array",
+    "set": "array",
+    "str": "string",
+    "unicode": "string",
+    "int": "number",
+    "float": "number",
+    "bool": "bool",
+    "NoneType": "null"
+}
+
+
+class QueryType(Enum):
+    SIMPLE = 0
+    SEARCH = 1
+    UNFOLD = 2
+
+
+def get_web_handle(browser_name):
+    try:
+        wb = webbrowser.get(using=browser_name)
+    except webbrowser.Error:
+        return None
+    return wb
+
+
+class Main(Wox):
+    def query(self, key):
+        result = []
+        query_type, paths = self.parse_input(key)
+
+        clipboard_text = str(pyperclip.paste())
+        text = self.parse_text(clipboard_text.strip())
+
+        json_dict = self.load_json(text)
+        if json_dict is None:
+            self.add_item(result, "can not read json from clipboard")
+            return result
+
+        if query_type == QueryType.SIMPLE:
+            self.add_item(result, text, text, subtitle="following rows are key-value pairs of clipboard object")
+
+        for i in range(len(paths)):
+            json_dict = self.load_json(text)
+            for k in sorted(json_dict.keys()):
+                v = json_dict[k]
+                if i != len(paths) - 1:
+                    # 中间的path都是整个的单词
+                    if k.lower() == paths[i].lower():
+                        text = v
+                        break
+                else:
+                    # 最后一个path模糊搜索
+                    if k.lower().find(paths[i].lower()) == -1:
+                        continue
+                    if k.lower() != paths[i].lower():
+                        self.add_item(result,
+                                      title=k + self.get_type_name(v) + ": " + self.generate_output(v),
+                                      output=self.build_predict_output(k, paths),
+                                      template=PREDICT_TEMPLATE,
+                                      subtitle="press enter to fill in the search box")
+                    else:
+                        text = v
+                        self.add_item(result,
+                                      title=k + self.get_type_name(v) + ": " + self.generate_output(v),
+                                      output=self.generate_output(v),
+                                      template=RESULT_TEMPLATE,
+                                      subtitle="press enter to show in web browser")
+
+        if len(result) == 0:
+            self.add_item(result, "Path error")
+
+        return result
+
+    def build_predict_output(self, k, paths):
+        path = ">".join(paths[:-1])
+        if path.strip() != "":
+            path = path + ">"
+        return "json " + path + k
+
+    def add_item(self, result: List[dict], title, output=None, template=None, subtitle=None):
+        if template is None:
+            template = RESULT_TEMPLATE
+        tmp_template = copy.deepcopy(template)
+        tmp_template['Title'] = tmp_template['Title'].format(title)
+        if subtitle is not None:
+            tmp_template['SubTitle'] = tmp_template['SubTitle'] = subtitle
+        tmp_template['JsonRPCAction']['parameters'][0] = str(output)
+        result.append(tmp_template)
+
+    def parse_input(self, key):
+        if key.strip() == "":
+            return QueryType.SIMPLE, [""]
+        return QueryType.SEARCH, key.strip().split('>')
+
+    def parse_text(self, text):
+        if text.startswith('\"'):
+            # like "{\"a\": 1}"
+            return eval(text)
+        elif text.startswith("{\\\""):
+            # like {\"a\": 1}
+            return eval("\"" + text + "\"")
+        else:
+            # like {"a": 1}
+            return text
+
+    def load_json(self, input):
+        if type(input) == dict:
+            return input
+        try:
+            json_obj = json.loads(input)
+        except Exception:
+            return None
+        return json_obj
+
+    def generate_output(self, param, intent=None):
+        if self.check_if_escape_json_dict(param):
+            param = self.load_json(param)
+        if type(param) is not str:
+            return json.dumps(param, ensure_ascii=False, indent=intent)
+        return param
+
+    def check_if_escape_json_dict(self, data):
+        if type(data) is not str:
+            return False
+        if not data.startswith('{'):
+            return False
+        try:
+            json.loads(data)
+        except Exception:
+            return False
+        return True
+
+    def create_file_and_open(self, data):
+        file_name = "data.json"
+        with open("./" + file_name, mode='w') as f:
+            f.write(data)
+        current_path = os.getcwd()
+        wb = get_web_handle('google-chrome')
+        if wb is None:
+            wb = get_web_handle('chrome')
+        if wb is None:
+            wb = get_web_handle('windows-default')
+        wb.open(current_path + "/" + file_name)
+        WoxAPI.change_query(current_path + "/" + file_name)
+
+    def change_query(self, text):
+        WoxAPI.change_query(text)
+
+    def get_type_name(self, item):
+        type_str = str(type(item))
+        pattern = re.compile("\'(.*?)\'")
+        py_type_name = pattern.search(type_str).group()[1:-1]
+        return '(' + PYTHON_JSON_TYPE_MAP.get(py_type_name, 'unknown') + ')'
+
+
+if __name__ == '__main__':
+    Main()
